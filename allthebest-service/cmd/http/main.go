f6ae581f9f5d191c705ef679c984d7e7d705338b
Pratik Mota <mota.pratik@gmail.com>
2023-03-29 00:28:51 +0530

    configuration options

--- allthebest-service/cmd/http/main.go
+++ allthebest-service/cmd/http/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	environment string
+)
+
 func main() {
+	flag.StringVar(&environment, "env", "", "Set the environment (testing, development, production)")
+	flag.Parse()
+
+	envVariablesFileName := fmt.Sprintf("../../config/%s.env", environment)
+	if err := godotenv.Load(envVariablesFileName); err != nil {
+		panic(fmt.Errorf("no .env file found: %s", err.Error()))
+	}
 	//Http.StartHttpService(container.Setup())
 }
--- allthebest-service/go.mod
+++ allthebest-service/go.mod
@@ -1,3 +1,5 @@
 module allthebest-backend/allthebest-service
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
