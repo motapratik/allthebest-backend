a9bdd122870a27fe4aedc995d6be09d5e8d2917d
Pratik Mota <pratik@pratikmota.com>
2023-06-03 01:32:00 +0530

    improvement in debug

--- allthebest-service/cmd/app/main.go
+++ allthebest-service/cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"allthebest-backend/allthebest-service/config"
 	"allthebest-backend/allthebest-service/pkg/api"
@@ -16,6 +17,9 @@ const CONFIG_PATH string = "./../../config"
 func main() {
 	flag.StringVar(&environment, "env", "", "Set the environment (testing, development, production)")
 	flag.Parse()
+	if (environment != "development") && (environment != "testing") && (environment != "production"){
+		panic(fmt.Errorf("enter valid environment name"))
+	}
 	// Load Configuration from JSON and ENV files
 	cfg, err := config.LoadConfig(environment, CONFIG_PATH)
 	if err != nil {
